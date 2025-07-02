04f5e092ef8e0c300160c1d5d9bed120ff70d89c
Udita Shukla <shuklaudita8@gmail.com>
2025-07-02 17:47:06 +0530

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -1,4 +1,4 @@
-// Smooth scroll for internal navigation links
+
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
